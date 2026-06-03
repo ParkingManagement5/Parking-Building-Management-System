9b1ff0a8099ec6c0fddfc30ea14434ea24476136
dibaooo <vodibao2904@gmail.com>
2026-06-03 16:39:58 +0700

    feat: init BE base structure with Auth module and DB schema

---
+++ parking-backend/src/main/java/com/swp391/parking/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.swp391.parking.repository;
+
+import com.swp391.parking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
