c07eeb49805d2d8de7a689835f59bb8113b10614
Sang <sangnpse190099@gmail.com>
2026-06-06 14:22:25 +0700

    feat: add PricingPolicyRepository

---
+++ parking-backend/src/main/java/com/swp391/parking/repository/PricingPolicyRepository.java
@@ -0,0 +1,9 @@
+package com.swp391.parking.repository;
+
+import com.swp391.parking.entity.PricingPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PricingPolicyRepository extends JpaRepository<PricingPolicy, Long> {
+}
