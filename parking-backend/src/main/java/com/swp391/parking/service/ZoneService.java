2804fbad5b8144798ae921bd63fd2dc64263546c
Du <honguyendugk@gmail.com>
2026-06-08 08:04:26 +0700

    feat: add parking structure services

---
+++ parking-backend/src/main/java/com/swp391/parking/service/ZoneService.java
@@ -0,0 +1,13 @@
+package com.swp391.parking.service;
+
+import com.swp391.parking.dto.request.ZoneRequest;
+import com.swp391.parking.entity.Zone;
+import java.util.List;
+
+public interface ZoneService {
+    List<Zone> getByFloor(Long floorId);
+    Zone getById(Long id);
+    Zone create(ZoneRequest req);
+    Zone update(Long id, ZoneRequest req);
+    void deactivate(Long id);
+}
