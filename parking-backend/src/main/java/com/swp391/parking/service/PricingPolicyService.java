2746e7ce3e236702ee068d7bfae2617cf7b2a7a7
Sang <sangnpse190099@gmail.com>
2026-06-10 21:28:26 +0700

    fix: update PricingPolicy to use VehicleType relationship, add activate API

--- parking-backend/src/main/java/com/swp391/parking/service/PricingPolicyService.java
+++ parking-backend/src/main/java/com/swp391/parking/service/PricingPolicyService.java
@@ -11,4 +11,5 @@ public interface PricingPolicyService {
     List<PricingPolicyResponse> getAllPolicies();
     PricingPolicyResponse updatePolicy(Long id, CreatePricingPolicyRequest request);
     void deletePolicy(Long id);
+     PricingPolicyResponse activatePolicy(Long id);
 }
