ae00993378f2a1684d93f3ff13ea106149127903
dibaooo <vodibao2904@gmail.com>
2026-06-13 12:46:46 +0700

    wip: integrate BE3 services and slot assignment

---
+++ parking-backend/src/main/java/com/swp391/parking/service/BookingService.java
@@ -0,0 +1,13 @@
+package com.swp391.parking.service;
+
+import com.swp391.parking.dto.request.CreateBookingRequest;
+import com.swp391.parking.dto.response.BookingResponse;
+import java.util.List;
+
+public interface BookingService {
+    BookingResponse createBooking(Long currentUserId, CreateBookingRequest request);
+    BookingResponse getBooking(Long bookingId);
+    List<BookingResponse> getMyBookings(Long currentUserId);
+    BookingResponse cancelBooking(Long bookingId, Long currentUserId);
+    BookingResponse confirmBookingAfterPayment(Long bookingId);
+}
