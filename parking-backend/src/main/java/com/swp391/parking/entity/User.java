a96870783bae61c7717e8d8c497cb74274d2eaf4
Sang <sangnpse190099@gmail.com>
2026-06-14 04:27:24 +0700

    feat: add Payment entity and repository

--- parking-backend/src/main/java/com/swp391/parking/entity/User.java
+++ parking-backend/src/main/java/com/swp391/parking/entity/User.java
@@ -22,7 +22,7 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private int userId;
+    private Integer userId;
 
     @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
