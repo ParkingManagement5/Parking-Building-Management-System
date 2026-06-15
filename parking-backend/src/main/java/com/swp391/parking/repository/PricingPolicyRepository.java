5767c0fb3cd8ac25720194750233c70a7db34699
Sang <sangnpse190099@gmail.com>
2026-06-16 00:39:18 +0700

    feat: add Request entity, repository, service, controller

--- parking-backend/src/main/java/com/swp391/parking/repository/PricingPolicyRepository.java
+++ parking-backend/src/main/java/com/swp391/parking/repository/PricingPolicyRepository.java
@@ -4,6 +4,7 @@ import com.swp391.parking.entity.PricingPolicy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PricingPolicyRepository extends JpaRepository<PricingPolicy, Long> {
 }
--- parking-backend/src/main/java/com/swp391/parking/repository/ShiftRepository.java
+++ parking-backend/src/main/java/com/swp391/parking/repository/ShiftRepository.java
@@ -4,6 +4,7 @@ import com.swp391.parking.entity.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
 }
