14493222a1c45a3f47f83752aa1e4b1c0b0af984
Sang <sangnpse190099@gmail.com>
2026-06-18 06:58:08 +0700

    feat: add ExceptionCase entity, repository, service

---
+++ parking-backend/src/main/java/com/swp391/parking/dto/response/ExceptionCaseResponse.java
@@ -0,0 +1,23 @@
+package com.swp391.parking.dto.response;
+
+import com.swp391.parking.entity.ExceptionCase.ExceptionStatus;
+import com.swp391.parking.entity.ExceptionCase.ExceptionType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ExceptionCaseResponse {
+
+    private Integer exceptionId;
+    private Integer sessionId;
+    private Integer requestId;
+    private ExceptionType exceptionType;
+    private String description;
+    private ExceptionStatus status;
+    private Integer resolvedBy;
+    private LocalDateTime resolvedAt;
+    private LocalDateTime createdAt;
+}
